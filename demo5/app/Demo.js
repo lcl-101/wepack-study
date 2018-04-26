6ed015a3f8bfa3a79275770b67d02918d73bc8c0
lichenglong <554675188@qq.com>
2018-04-26 17:59:45 +0800

    demo5

---
+++ demo5/app/Demo.js
@@ -0,0 +1,14 @@
+import './app.css';
+import React, {Component} from 'react';
+
+class Demo extends Component{
+  render() {
+    return (
+      <h1 className="demo">
+        demo5
+      </h1>
+    )
+  }
+}
+
+export default Demo;
--- demo5/webpack.config.js
+++ demo5/webpack.config.js
@@ -1,3 +1,31 @@
 module.exports = {
-  entry:
+  entry:__dirname + '/app/main.js',
+  output: {
+    path:__dirname + '/app',
+    filename:'bundle.js'
+  },
+  devServer:{
+    contentBase:'./app',
+    historyApiFallback: true,
+    inline: true,
+    open: true
+  },
+  module:{
+    rules:[
+      {
+        test:/(\.jsx|\.js)$/,
+        use:{
+          loader:'babel-loader',
+          options:{
+            presets:['env','react']
+          }
+        },
+        exclude:/node_modules/
+      },
+      {
+        test:/\.css$/,
+        use:['style-loader','css-loader']
+      }
+    ]
+  }
 }
