4c28b3c2a5565a8c4650d5f51226391cba07abad
lichenglong <554675188@qq.com>
2018-04-27 19:05:22 +0800

    ++

--- demo6/webpack.config.js
+++ demo6/webpack.config.js
@@ -1,4 +1,23 @@
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+const webpack = require('webpack');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');  //代码压缩
+const UglifyJSPlugins = new UglifyJSPlugin({
+  uglifyOptions:{
+    warnings: false,
+    ie8: true,
+  }
+});
+
+const DefinePlugin = new webpack.DefinePlugin({
+   __DEV__: JSON.stringify(JSON.parse(process.env.DEBUG || 'true'))
+})
+console.log(DefinePlugin.definitions);
+console.log(process.env.NODE_ENV);
+console.log(process.env);
+if(DefinePlugin.definitions.__DEV__){
+  console.log('this is dev env');
+}else {
+  console.log('this is prod env');
+}
 
 module.exports = {
   entry: __dirname + '/app/main.js',
@@ -41,11 +60,6 @@ module.exports = {
     ]
   },
   plugins:[
-    new UglifyJSPlugin({
-      uglifyOptions:{
-        warnings: false,
-        ie8: true,
-      }
-    })
+    UglifyJSPlugins
   ]
 }
